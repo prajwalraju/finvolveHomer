80c5260be954afbc91f74de7fda2a49aa2dc8a5a
prajwalraju <prajwalrajup@gmail.com>
2023-11-15 16:26:13 +0530

    added google analytics

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { createApp, h } from "vue";
 import App from "./App.vue";
+import VueGtag from "vue-gtag";
 
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./assets/app.scss";
 
 const app = createApp(App);
+
+app.use(VueGtag, {
+  config: { id: "G-8NDN5YHXEG" }
+})
 
 app.component("DynamicStyle", (_props, context) => {
   return h("style", {}, context.slots);
